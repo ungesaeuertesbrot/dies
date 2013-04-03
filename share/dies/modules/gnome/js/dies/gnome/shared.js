b31e9e766e04c410409c16cac189c6a8df78eda4
Thorsten <thorsten@BABYLON.lan>
2013-04-03 11:21:39 +0200

    * Updated module name in shared.js

--- share/dies/modules/gnome/js/dies/gnome/shared.js
+++ share/dies/modules/gnome/js/dies/gnome/shared.js
@@ -1,7 +1,7 @@
 const GLib = imports.gi.GLib;
 const Context = imports.malus.context;
 
-const MODULE_NAME = "gui_gnome";
+const MODULE_NAME = "gnome";
 
 var module_dir = Context.modules.get_module_directory (MODULE_NAME);
 var ui_dir = GLib.build_filenamev ([module_dir, "ui"]);
